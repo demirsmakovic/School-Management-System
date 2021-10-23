bc197627cddf6c2a9f587e25b618d2e1d34de720
demirsmakovic <demir.smakovic@gmail.com>
2021-10-23 23:37:20 +0200

    Added options for edit, update and delete user

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -32,8 +32,48 @@ class UserController extends Controller
         $data->email = $request->email;
         $data->password = bcrypt($request->password);
         $data->save();
-        
-        return Redirect()->route('user.view');
 
+        $notification = array(
+            'message' => 'User Inserted Successfull',
+            'alert-type' => 'success'
+        );
+        
+        return Redirect()->route('user.view')->with($notification);
+
+    }
+
+    public function UserEdit($id)
+    {
+        $user = User::find($id);
+        return view('backend.user.edit_user', compact('user'));
+    }
+
+    public function UserUpdate(Request $request, $id)
+    {
+        $data = User::find($id);
+        $data->usertype = $request->usertype;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->update();
+
+        $notification = array(
+            'message' => 'User Updated Successfull',
+            'alert-type' => 'success'
+        );
+        
+        return Redirect()->route('user.view')->with($notification);
+
+    }
+
+    public function UserDelete($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        $notification = array(
+            'message' => 'User Deleted Successfull',
+            'alert-type' => 'danger'
+        );
+        
+        return Redirect()->route('user.view')->with($notification);
     }
 }
---
+++ resources/views/backend/user/edit_user.blade.php
@@ -0,0 +1,91 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <div class="container-full">
+      	  
+
+      <!-- Main content -->
+      <section class="content">
+
+       <!-- Basic Forms -->
+        <div class="box">
+          <div class="box-header with-border">
+            <h4 class="box-title">Edit User</h4>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div class="row">
+              <div class="col">
+                  <form action="{{ route('user.update', $user->id) }}" method="POST">
+                    @csrf
+                    <div class="row">
+                      <div class="col-12">
+                          
+                        <div class="row">
+
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <h5>User Role<span class="text-danger">*</span></h5>
+                                    <div class="controls">
+                                        <select name="usertype" id="select" required class="form-control">
+                                            <option value="" selected="" disabled="">Select Role</option>
+                                            <option value="Admin" {{ $user->usertype == "Admin" ? "selected" : "" }}>Admin</option>
+                                            <option value="User"  {{ $user->usertype == "User" ? "selected" : "" }}>User</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                               <div class="form-group">
+                                  <h5>User Name <span class="text-danger">*</span></h5>
+                                  <div class="controls">
+                                  <input type="text" name="name" class="form-control" value="{{ $user->name }}" required data-validation-required-message="This field is required"> </div>
+                                </div>
+                            </div>
+
+                        </div>
+                          
+                    <div class="row">
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <h5>User Email<span class="text-danger">*</span></h5>
+                                <div class="controls">
+                                    <input type="email" name="email" class="form-control" value="{{ $user->email }}" required data-validation-required-message="This field is required"> </div>
+                            </div>
+                        </div>
+
+                        
+                      </div>
+                    
+                          
+                      <div class="text-xs-right">
+                          <input type="submit" class="btn btn-rounded btn-info" value="Update">
+                      </div>
+                  </form>
+
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+
+      </section>
+      <!-- /.content -->
+    </div>
+</div>
+<!-- /.content-wrapper -->
+
+
+
+
+
+
+
+@endsection
